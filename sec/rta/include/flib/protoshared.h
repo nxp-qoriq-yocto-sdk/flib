121d327ce54c2f2ef46161ad5db736e528c394f3
Alex Porosanu <alexandru.porosanu@nxp.com>
2016-04-05 10:43:28 +0300

    fix MBMS payload CRC calculation
    The CRC result depends on the initial conditions, i.e.
    the initial value of the flip-flops in the LFSR. The 3GPP TS 25.446
    standard does not specify neither the initial value(s) or the
    operations that happen before or after the CRC calculation is done
    (f.i. input bit-swap, output complement etc.).
    As a result of interoperability tests conducted by Nokia/ALU, the
    conclusion that the initial value of the LFSR needs to be set to
    0 prior to starting the CRC calculation.
    
    This patch changes the SEC descriptor to set the initial value
    in the LFSR to 0 for MBMS payload checksum calculation.
    This applies only for MBMS Type 1 & 3 PDUs.
    
    Signed-off-by: Alex Porosanu <alexandru.porosanu@nxp.com>

--- sec/rta/include/flib/protoshared.h
+++ sec/rta/include/flib/protoshared.h
@@ -5060,6 +5060,7 @@ static inline unsigned cnstr_shdsc_mbms_type1_3(uint32_t *descbuf,
 	/* Request the CRC engine */
 	ALG_OPERATION(OP_ALG_ALGSEL_CRC,
 		      OP_ALG_AAI_CUST_POLY |
+		      OP_ALG_AAI_IVZ |
 		      OP_ALG_AAI_DIS | OP_ALG_AAI_DOS | OP_ALG_AAI_DOC,
 		      OP_ALG_AS_INITFINAL, ICV_CHECK_DISABLE,
 		      OP_ALG_ENCRYPT);
